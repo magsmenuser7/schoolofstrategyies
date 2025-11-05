ff0729a5baa69fb7e7009d254e6ef2983f7ea7b7
magsmenuser7 <magsmenuser7@gmail.com>
2025-11-05 13:44:55 +0530

    changes done

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.DEV ? "/schoolofstrategy" : "/"}>
+      <BrowserRouter basename={import.meta.env.DEV ? "/schoolofstrategyies" : "/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
